a47fb7f05c30f5f9ae2f20bad2e322a1709fab26
aoxc <orcunkundakci@gmail.com>
2026-03-03 16:43:15 +0300

    :package: chore(deps): align OpenZeppelin submodules to v5.0.0
    :gear: Initialize forge-std and upgradeable frameworks for Neural Core.

--- lib/forge-std/test/compilation/CompilationTestBase.sol
+++ lib/forge-std/test/compilation/CompilationTestBase.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
 pragma solidity >=0.8.13 <0.9.0;
 
-import {TestBase} from "../../src/Test.sol";
+import {TestBase} from  "aoxc/Test.sol";
 
 // The purpose of this contract is to benchmark compilation time to avoid accidentally introducing
 // a change that results in very long compilation times with via-ir. See https://github.com/foundry-rs/forge-std/issues/207
